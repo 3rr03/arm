2e345dae285adda3391ea2e150a04d94e5b4b093
soofoo <soofoo@tp240>
2018-05-14 18:20:01 +0200

    Ein paar Sachen angelegt zum test

---
+++ test.c
@@ -0,0 +1,12 @@
+/* Dies ist ein Test um die Versionskontrolle von git zu testen
+ *
+ * Ein kleiner Mehrzeiliger Kommentar
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    printf("Dies ist ein Test");
+}
